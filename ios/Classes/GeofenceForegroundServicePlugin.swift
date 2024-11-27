61f42e54e2448dead9c1c31bbc9aaeca7d6a1874
Yarith <yarith.student@gmail.com>
2024-11-27 21:21:18 +0700

    remove ios impl

--- ios/Classes/GeofenceForegroundServicePlugin.swift
+++ ios/Classes/GeofenceForegroundServicePlugin.swift
@@ -16,22 +16,22 @@ public class GeofenceForegroundServicePlugin: NSObject, FlutterPlugin {
     private var result: FlutterResult?
 
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let instance = GeofenceForegroundServicePlugin()
-
-        instance.locationManager.delegate = instance
-
-        instance.locationManager.requestAlwaysAuthorization()
-
-        instance.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        instance.locationManager.distanceFilter = 1.0
-
-        let channel = FlutterMethodChannel(
-            name: "\(GeofenceForegroundServicePlugin.identifier)/foreground_geofence_foreground_service",
-            binaryMessenger: registrar.messenger()
-        )
-
-        registrar.addMethodCallDelegate(instance, channel: channel)
-        registrar.addApplicationDelegate(instance)
+//        let instance = GeofenceForegroundServicePlugin()
+//
+//        instance.locationManager.delegate = instance
+//
+//        instance.locationManager.requestAlwaysAuthorization()
+//
+//        instance.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        instance.locationManager.distanceFilter = 1.0
+//
+//        let channel = FlutterMethodChannel(
+//            name: "\(GeofenceForegroundServicePlugin.identifier)/foreground_geofence_foreground_service",
+//            binaryMessenger: registrar.messenger()
+//        )
+//
+//        registrar.addMethodCallDelegate(instance, channel: channel)
+//        registrar.addApplicationDelegate(instance)
     }
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
@@ -57,7 +57,7 @@ public class GeofenceForegroundServicePlugin: NSObject, FlutterPlugin {
 
             result(true)
         case "stopGeofencingService":
-            locationManager.allowsBackgroundLocationUpdates = false
+//            locationManager.allowsBackgroundLocationUpdates = false
 //            locationManager.pausesLocationUpdatesAutomatically = true
 //            locationManager.stopUpdatingLocation()
 
@@ -150,48 +150,48 @@ public class GeofenceForegroundServicePlugin: NSObject, FlutterPlugin {
     }
 }
 
-extension GeofenceForegroundServicePlugin: CLLocationManagerDelegate {
-    // Implement CLLocationManagerDelegate methods here
-    // For example:
-
-    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // Handle location updates here
-    }
-
-    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        // Handle location manager errors here
-    }
-
-    public func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        print("Entered geofence: \(region.identifier)")
-        // Perform actions when entering the geofence
-        eventHandler(zoneID: region.identifier, triggerType: 1)
-    }
-
-    public func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
-        print("Exited geofence: \(region.identifier)")
-        // Perform actions when exiting the geofence
-        eventHandler(zoneID: region.identifier, triggerType: 2)
-    }
-
-    public func eventHandler(zoneID: String, triggerType: Int) {
-        guard
-            let callbackHandle = UserDefaultsHelper.getStoredCallbackHandle(),
-            let _ = FlutterCallbackCache.lookupCallbackInformation(callbackHandle)
-        else {
-//            logError("[\(String(describing: self))] \(GFSError.pluginNotInitialized.message)")
-            return
-        }
-
-        let operationQueue = OperationQueue()
-        // Create an operation that performs the main part of the background task
-        let operation = BackgroundTaskOperation(
-            zoneID: zoneID,
-            triggerType: triggerType,
-            flutterPluginRegistrantCallback: GeofenceForegroundServicePlugin.flutterPluginRegistrantCallback
-        )
-
-        // Start the operation
-        operationQueue.addOperation(operation)
-    }
-}
+//extension GeofenceForegroundServicePlugin: CLLocationManagerDelegate {
+//    // Implement CLLocationManagerDelegate methods here
+//    // For example:
+//
+//    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        // Handle location updates here
+//    }
+//
+//    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+//        // Handle location manager errors here
+//    }
+//
+//    public func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+//        print("Entered geofence: \(region.identifier)")
+//        // Perform actions when entering the geofence
+//        eventHandler(zoneID: region.identifier, triggerType: 1)
+//    }
+//
+//    public func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+//        print("Exited geofence: \(region.identifier)")
+//        // Perform actions when exiting the geofence
+//        eventHandler(zoneID: region.identifier, triggerType: 2)
+//    }
+//
+//    public func eventHandler(zoneID: String, triggerType: Int) {
+//        guard
+//            let callbackHandle = UserDefaultsHelper.getStoredCallbackHandle(),
+//            let _ = FlutterCallbackCache.lookupCallbackInformation(callbackHandle)
+//        else {
+////            logError("[\(String(describing: self))] \(GFSError.pluginNotInitialized.message)")
+//            return
+//        }
+//
+//        let operationQueue = OperationQueue()
+//        // Create an operation that performs the main part of the background task
+//        let operation = BackgroundTaskOperation(
+//            zoneID: zoneID,
+//            triggerType: triggerType,
+//            flutterPluginRegistrantCallback: GeofenceForegroundServicePlugin.flutterPluginRegistrantCallback
+//        )
+//
+//        // Start the operation
+//        operationQueue.addOperation(operation)
+//    }
+//}
